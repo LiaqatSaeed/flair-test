d2bc8e854c772c0d4b5f9822d2cd4ee7680b091c
Liaqat Saeed <liaqatsaeed007@gmail.com>
2021-09-26 21:32:38 +0500

    Css for deployment

---
+++ flair-test/_next/static/CNPKxB_tiL4Ih-OmD5Eiw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/136-716f257ca1caba5276ce.js","static/chunks/pages/index-1fac6fa8976ff50b60a9.js","static/css/18ce3a4832ab6f938718.css"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
